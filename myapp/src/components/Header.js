e4404b6b65e6c03cdf4c68e81709f517ef7c6090
Gaddampraveen454 <gaddampraveen454@gmail.com>
2024-12-03 12:12:59 +0530

    updated

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -9,24 +9,12 @@ const Header = ({children, theme, setTheme}) => {
                 <span>{children}</span>
             </span>
             <span className="themeSelector">
-                <span 
-                  onClick={() => setTheme("light")} 
-                  className={ theme === "light" ? "light activeTheme" : "light"}></span>
-                <span 
-                  onClick={() => setTheme("medium")} 
-                  className={ theme === "medium" ? "medium activeTheme" : "medium"}></span>
-                <span 
-                  onClick={() => setTheme("dark")} 
-                  className={ theme === "dark" ? "dark activeTheme" : "dark"}></span>
-                <span 
-                  onClick={() => setTheme("gOne")} 
-                  className={ theme === "gOne" ? "gOne activeTheme" : "gOne"}></span>
-                <span 
-                  onClick={() => setTheme("gTwo")} 
-                  className={ theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}></span>
-                <span 
-                  onClick={() => setTheme("gThree")} 
-                  className={ theme === "gThree" ? "gThree activeTheme" : "gThree"}></span>
+                <span className= "light ActivTheme"></span>
+                <span className="medium"></span>
+                <span  className="dark"></span>
+                <span className="gOne"></span>
+                <span className= "gTwo"></span>
+                <span className="gThree"></span>
             </span>
         </header>
     );
