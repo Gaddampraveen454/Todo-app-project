b070b1fc794eed8d55c1fde968cac46b38b103ac
Gaddampraveen454 <gaddampraveen454@gmail.com>
2024-12-19 11:14:24 +0530

    updated

---
+++ myapp/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './app.css';
+import Header from './components/Header';
+import AddTask from './components/AddTask';
+import ShowTask from './components/ShowTask'
+
+const App = () => {
+  const [tasklist, setTasklist] = useState([]);
+  const [editTaskId, setEditTaskId] = useState(null); 
+  
+  return (
+    //hello this is gaddam praveen patel
+    <div className='App'>
+      <Header />
+      <AddTask 
+        tasklist={tasklist} 
+        setTasklist={setTasklist} 
+        editTaskId={editTaskId} 
+        setEditTaskId={setEditTaskId} 
+      />
+      <ShowTask 
+        tasklist={tasklist} 
+        setTasklist={setTasklist} 
+        setEditTaskId={setEditTaskId} 
+      />
+    </div>
+  );
+};
+
+export default App;
