0993937957bfbaedab38b40a67c19ebe035c0934
Gaddampraveen454 <gaddampraveen454@gmail.com>
2024-12-19 11:18:40 +0530

    task updated

---
+++ myapp/src/components/AddTask.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+const AddTask = ({ tasklist, setTasklist, editTaskId, setEditTaskId }) => {
+  const [inputValue, setInputValue] = useState(''); 
+
+  
+  useEffect(() => {
+    if (editTaskId) {
+      const taskToEdit = tasklist.find(task => task.id === editTaskId);
+      setInputValue(taskToEdit.name); 
+    } else {
+      setInputValue(''); 
+    }
+  }, [editTaskId, tasklist]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const date = new Date();
+
+    if (inputValue.trim() === '') return; 
+
+    if (editTaskId) {
+
+      const updatedTaskList = tasklist.map(task => 
+        task.id === editTaskId ? { ...task, name: inputValue, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` } : task
+      );
+      setTasklist(updatedTaskList);
+      setEditTaskId(null); 
+    } else {
+      
+      const newTask = {
+        id: date.getTime(),
+        name: inputValue, 
+        time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}` 
+      };
+      setTasklist([...tasklist, newTask]);
+    }
+
+
+    setInputValue('');
+  };
+
+  return (
+    <section className='addTask'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue} 
+          onChange={(e) => setInputValue(e.target.value)} 
+          autoComplete='off'
+          placeholder='ADD THE TASK'
+        />
+        <button type="submit">{editTaskId ? 'Update' : 'Add'}</button>
+      </form>
+    </section>
+  );
+};
+
+export default AddTask;
